8d22bff11bf514c7b698e51b34a5f26926bc4532
Halim <abdulhalimibnbyrd@gmail.com>
2023-03-22 09:27:28 -0400

    created first react component (:

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+
+
+function Greeting() {
+    return <h1>HELLO WORLD </h1>;
+    
+  }
+    
+ 
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+  root.render(<Greeting></Greeting>)
