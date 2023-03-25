a39e0aa6d09cbb7e790c8fe97d67ba7a7bb66dc3
Halim <abdulhalimibnbyrd@gmail.com>
2023-03-25 09:17:25 -0400

    learning react

--- src/index.js
+++ src/index.js
@@ -1,16 +1,38 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 
+/* challenge:
+
+Part 1:  Create a page of your own using a custom Page component
+
+It should return an ordered list with the reasons why you're
+excited to be learning React
 
 
-function Greeting() {
-    return <h1>HELLO WORLD </h1>;
-    
-  }
-    
- 
-  const root = ReactDOM.createRoot(document.getElementById('root'));
+*/
 
 
-  root.render(<Greeting></Greeting>)
+
+
+const WhyLearnReact = () => {
+  return (
+        <ol>
+            <li>Its popular</li>
+            <li>easier to build web applications</li>
+            <li> I CAN GET A JOB (: </li>
+        </ol>
+  )
+}
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+root.render(
+    <WhyLearnReact/>
+)
+
+
+
