2ae26628927bbfaa9e0d8800b5b57ece7f06caad
Halim <abdulhalimibnbyrd@gmail.com>
2023-04-06 08:34:48 -0400

    back road application setup

--- src/index.js
+++ src/index.js
@@ -1,54 +1,13 @@
-/* challenge:
-
-Part 1:  Create a page of your own using a custom Page component
-
-It should return an ordered list with the reasons why you're
-excited to be learning React
-
-
-*/
-
-
+import React from 'react';
 import ReactDom from 'react-dom/client';
-import './styles.css'
-
-
-import Header from './Header.js'
-import MainContent from './MainContent';
-import Footer from './Footer';
-
-
-
-
-
-
-const App = () => {
-   return (
-    <>
-      <Header/>
-      <MainContent/>
-      <Footer/>
-  </>
-   )
-}
-
-
-
-
-
+import App from './App';
 
 
 
 const root = ReactDom.createRoot(document.getElementById('root'));
 
-root.render(
-<> 
-
-  <App/>
-
-  </>
-
+root.render (
+    <>
+        <App/>
+    </>
 )
-
-
-
